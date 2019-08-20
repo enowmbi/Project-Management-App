8ea8c03086b45bf3377feaab7699ab5a5f0095ee
Enow Mbi <benowmbi@yahoo.com>
2019-08-20 20:01:37 +0100

    Add project model

---
+++ app/models/project.rb
@@ -0,0 +1,9 @@
+class Project < ApplicationRecord
+  has_many :issues, dependent: :destroy
+
+  validates :name, presence: true
+  validates :name, uniqueness: true
+  validates :description, presence: true
+  validates :duration, numericality: {only_integer: true, greater_than_or_equal_to: 1}
+  validates :status, presence: true
+end
