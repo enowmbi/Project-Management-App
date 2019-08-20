ec87388cf36585e042ebe208c6bbfa961aa1dc67
Enow Mbi <benowmbi@yahoo.com>
2019-08-20 20:41:41 +0100

    Add create issue

---
+++ db/migrate/20190408115239_create_issues.rb
@@ -0,0 +1,12 @@
+class CreateIssues < ActiveRecord::Migration[5.2]
+  def change
+    create_table :issues do |t|
+      t.string :title
+      t.text :description
+      t.string :severity
+      t.string :status
+
+      t.timestamps
+    end
+  end
+end
