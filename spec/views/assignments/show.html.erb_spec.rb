f2d17be5232e9a1ee50c17984659324b0deacddd
Enow Mbi <benowmbi@yahoo.com>
2019-08-21 03:04:19 +0100

    Add assignment view spec

---
+++ spec/views/assignments/show.html.erb_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe "assignments/show", type: :view do
+  before(:each) do
+    @assignment = assign(:assignment, Assignment.create!(
+      :user => nil,
+      :project => nil,
+      :status => "Status"
+    ))
+  end
+
+  it "renders attributes in <p>" do
+    render
+    expect(rendered).to match(//)
+    expect(rendered).to match(//)
+    expect(rendered).to match(/Status/)
+  end
+end
