be5bfb50b76a4a2cf2a72352ad23cf8e0e57415e
Enow Mbi <benowmbi@yahoo.com>
2019-08-21 03:14:54 +0100

    Modify routes with resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  resources :assignments
+  devise_for :users
+  resources :issues
+  resources :projects
   root "home#index"
   get 'home/index'
   resources :employees
+  resources :users, only: [:show]
 end
