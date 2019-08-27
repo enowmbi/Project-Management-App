d0e5391a12ae8317b5b812fba9890137f354e9bf
Enow Mbi <benowmbi@yahoo.com>
2019-08-27 20:37:53 +0100

    Modify projects factory - added dash inbetween name and number

--- spec/factories/projects.rb
+++ spec/factories/projects.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do 
   factory :project do 
-    sequence(:name) {|n| "project #{n}"}
+    sequence(:name) {|n| "project-#{n}"}
     description {"This is a description of #{:name}"}
     duration {1}
     status {"Deployed"}
