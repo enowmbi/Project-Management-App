2f1e2149e7c39540e50bcfefad14012a2941c2f5
Enow Mbi <benowmbi@yahoo.com>
2020-04-09 13:18:12 +0100

    Modify assignments_controller.rb --change issue_id to task_id in whitelisted params

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -69,6 +69,6 @@ class AssignmentsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def assignment_params
-      params.require(:assignment).permit(:user_id, :issue_id, :assigned_on, :status, :resolved_on)
+      params.require(:assignment).permit(:user_id, :task_id, :assigned_on, :status, :resolved_on)
     end
 end
