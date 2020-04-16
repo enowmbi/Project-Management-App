8b1bcc54b8289c8f1be3182f4792f952ae8708bc
Enow Mbi <benowmbi@yahoo.com>
2020-04-16 16:21:32 +0100

    Modify team.rb --add default scope to display teams in descending order of created_at column

--- app/models/team.rb
+++ app/models/team.rb
@@ -2,4 +2,7 @@ class Team < ApplicationRecord
   has_many :memberships, dependent: :destroy
   has_many :users, through: :memberships
   has_many :projects
+
+  default_scope ->{order(created_at: :desc)}
+
 end
