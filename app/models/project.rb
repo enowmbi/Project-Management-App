6accbfa7b977fd605c9bece25e8625ac0cc8f6bd
Enow Mbi <benowmbi@yahoo.com>
2020-04-16 16:20:34 +0100

    Modify project.rb --add default scope to display projects in descender order of created_at date

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,6 @@
 class Project < ApplicationRecord
   has_many :tasks, dependent: :destroy
-  belongs_to :user
+  belongs_to :user, counter_cache: true
   belongs_to :team
 
   validates :name, presence: true
@@ -8,4 +8,6 @@ class Project < ApplicationRecord
   validates :description, presence: true
   validates :duration, numericality: {only_integer: true, greater_than_or_equal_to: 1}
   validates :status, presence: true
+
+  default_scope ->{order(created_at: :desc)}
 end
