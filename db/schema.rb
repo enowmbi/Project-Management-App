79d2a5eaaa35880291cb1414e498ff8eff410cd9
Enow Mbi <benowmbi@yahoo.com>
2020-04-16 10:25:25 +0100

    Modify schema.rb --remove user_id from teams and associated index

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_16_090227) do
+ActiveRecord::Schema.define(version: 2020_04_16_091521) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,10 +59,8 @@ ActiveRecord::Schema.define(version: 2020_04_16_090227) do
 
   create_table "teams", force: :cascade do |t|
     t.string "name"
-    t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["user_id"], name: "index_teams_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -88,5 +86,4 @@ ActiveRecord::Schema.define(version: 2020_04_16_090227) do
   add_foreign_key "projects", "teams"
   add_foreign_key "projects", "users"
   add_foreign_key "tasks", "projects"
-  add_foreign_key "teams", "users"
 end
