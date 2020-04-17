f87039c64bd262ba27b3af29c498637ac12b4932
Enow Mbi <benowmbi@yahoo.com>
2020-04-17 21:38:22 +0100

    Modify team.rb --add dependent: :destroy(allowing projects to be destroyed when team is destroyed) --add 'accepts_nested_attributes_for :projects'

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,7 +1,9 @@
 class Team < ApplicationRecord
   has_many :memberships, dependent: :destroy
   has_many :users, through: :memberships
-  has_many :projects
+  has_many :projects, dependent: :destroy
+
+  accepts_nested_attributes_for :projects, allow_destroy: true,reject_if: :all_blank
 
   default_scope ->{order(created_at: :desc)}
 
