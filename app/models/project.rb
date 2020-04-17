cad01f650c83e079a9b7f624ac44e3040e3b005e
Enow Mbi <benowmbi@yahoo.com>
2020-04-17 19:30:58 +0100

    Modify project.rb --add 'allow_nested_attributes_for :tasks'

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,6 +2,7 @@ class Project < ApplicationRecord
   has_many :tasks, dependent: :destroy
   belongs_to :user, counter_cache: true
   belongs_to :team
+  accepts_nested_attributes_for :tasks,reject_if: :all_blank,allow_destroy: true
 
   validates :name, presence: true
   validates :name, uniqueness: true
