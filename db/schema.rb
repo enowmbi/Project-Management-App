46b280b480d069050cb241f3946f40dd68d26d3b
Enow Mbi <benowmbi@yahoo.com>
2020-04-17 19:50:57 +0100

    Create and run 'add_due_date_to_projects' migration --modify schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_17_184432) do
+ActiveRecord::Schema.define(version: 2020_04_17_184939) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -72,6 +72,7 @@ ActiveRecord::Schema.define(version: 2020_04_17_184432) do
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
     t.bigint "team_id", null: false
+    t.date "due_date"
     t.index ["team_id"], name: "index_projects_on_team_id"
     t.index ["user_id"], name: "index_projects_on_user_id"
   end
