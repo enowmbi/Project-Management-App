e12e7c28a25422b667c9bfeee68d2860a20c0607
Enow Mbi <benowmbi@yahoo.com>
2020-04-17 19:47:13 +0100

    Create and run 'remove_duration_from_projects' migration --modify schema in the process

---
+++ db/migrate/20200417184432_remove_duration_from_projects.rb
@@ -0,0 +1,5 @@
+class RemoveDurationFromProjects < ActiveRecord::Migration[6.0]
+  def change
+    remove_column :projects, :duration
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_17_184156) do
+ActiveRecord::Schema.define(version: 2020_04_17_184432) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -67,7 +67,6 @@ ActiveRecord::Schema.define(version: 2020_04_17_184156) do
   create_table "projects", force: :cascade do |t|
     t.string "name"
     t.text "description"
-    t.integer "duration"
     t.string "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
