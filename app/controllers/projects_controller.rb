ea4f22e1a896c7dc27f16814bb180adc44ef266f
Enow Mbi <benowmbi@yahoo.com>
2020-04-18 13:37:17 +0100

    Modify projects_controller --cleanup controller --remove commented code lines

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,11 +1,10 @@
 class ProjectsController < ApplicationController
   before_action :set_project, only: [:show, :edit, :update, :destroy]
-  before_action :set_teams, only: [:new, :edit]
 
   # GET /projects
   # GET /projects.json
   def index
-    @projects = current_user.projects
+    @projects = Project.all
   end
 
   # GET /projects/1
@@ -15,19 +14,17 @@ class ProjectsController < ApplicationController
 
   # GET /projects/new
   def new
-    @project = current_user.projects.build
     @project.tasks.build
   end
 
   # GET /projects/1/edit
   def edit
-    @project.tasks.build
   end
 
   # POST /projects
   # POST /projects.json
   def create
-    @project = current_user.projects.build(project_params)
+    @project = Project.new(project_params)
 
     respond_to do |format|
       if @project.save
@@ -68,11 +65,6 @@ class ProjectsController < ApplicationController
   # Use callbacks to share common setup or constraints between actions.
   def set_project
     @project = Project.find(params[:id])
-  end
-
-  #Set the current user's teams to be used when creating and editing projects
-  def set_teams
-    @teams =  current_user.teams
   end
 
   # Never trust parameters from the scary internet, only allow the white list through.
