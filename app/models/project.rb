7a80c3d4a4be5201b0bd24a3db88195c9b8f1bf9
Enow Mbi <benowmbi@yahoo.com>
2020-04-18 13:33:02 +0100

    Modify project model --remove belongs_to :user association and remove duration validation

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,13 +1,12 @@
 class Project < ApplicationRecord
   has_many :tasks, dependent: :destroy
-  belongs_to :user, counter_cache: true
+  # belongs_to :user, counter_cache: true
   belongs_to :team
   accepts_nested_attributes_for :tasks,reject_if: :all_blank,allow_destroy: true
 
   validates :name, presence: true
   validates :name, uniqueness: true
   validates :description, presence: true
-  validates :duration, numericality: {only_integer: true, greater_than_or_equal_to: 1}
   validates :status, presence: true
 
   default_scope ->{order(created_at: :desc)}
