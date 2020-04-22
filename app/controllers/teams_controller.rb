a525d4c4a98b9efbceecef5cacf3376132bcbbe0
Enow Mbi <benowmbi@yahoo.com>
2020-04-22 03:01:39 +0100

    Modify teams controller to find by friendly id

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -64,7 +64,7 @@ class TeamsController < ApplicationController
   private
   # Use callbacks to share common setup or constraints between actions.
   def set_team
-    @team = Team.includes(:memberships).find(params[:id])
+    @team = Team.includes(:memberships).friendly.find(params[:id])
   end
 
   # Only allow a list of trusted parameters through.
