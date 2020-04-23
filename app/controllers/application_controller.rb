a7e061afe4f5a3e345b0515047572a25eaddd2bd
Enow Mbi <benowmbi@yahoo.com>
2020-04-23 13:56:04 +0100

    Remove first_name and last_name from allowable keys during create and update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,13 +3,11 @@ before_action :configure_permitted_parameters,if: :devise_controller?
 
   protected
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up,keys: [:first_name])
-    devise_parameter_sanitizer.permit(:sign_up,keys: [:last_name])
+    devise_parameter_sanitizer.permit(:sign_up,keys: [:name])
     devise_parameter_sanitizer.permit(:sign_up,keys: [:phone])
     devise_parameter_sanitizer.permit(:sign_up,keys: [:is_admin])
 
-    devise_parameter_sanitizer.permit(:account_update,keys: [:first_name])
-    devise_parameter_sanitizer.permit(:account_update,keys: [:last_name])
+    devise_parameter_sanitizer.permit(:account_update,keys: [:name])
     devise_parameter_sanitizer.permit(:account_update,keys: [:phone])
     devise_parameter_sanitizer.permit(:account_update,keys: [:is_admin])
   end
