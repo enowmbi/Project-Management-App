59f2616fa43c4dbdddf817dd21be99403056507e
Enow Mbi <benowmbi@yahoo.com>
2020-04-24 11:12:33 +0100

    Add project_activity and project_status methods to projects_helper.rb

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,2 +1,9 @@
 module ProjectsHelper
+  def project_activity(project)
+     project.active? ? 'Active' : 'Inactive'
+  end
+
+  def project_status(project)
+     project.complete? ? 'Complete' : 'Incomplete'
+  end
 end
