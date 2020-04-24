e5449a3280a1c34253a4cca9cc699c796b7ea5b8
Enow Mbi <benowmbi@yahoo.com>
2020-04-24 05:08:48 +0100

    Whitelist active attribute

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -75,7 +75,7 @@ class ProjectsController < ApplicationController
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def project_params
-    params.require(:project).permit(:name, :description, :due_date, :status, :team_id)
+    params.require(:project).permit(:name, :description, :due_date, :status, :team_id, :active)
   end
 
 
