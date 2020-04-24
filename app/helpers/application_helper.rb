c3fef2fde37249650233b961ce2bd8943e87371a
Enow Mbi <benowmbi@yahoo.com>
2020-04-24 17:45:31 +0100

    Add user_gravatar method to get user gravatar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,30 @@
 module ApplicationHelper
+  require 'net/http'
+  require 'uri'
+
+  DEFAULT_GRAVATAR = 'default_gravatar.jpeg'
+
   def task_status(task)
-     task.complete? ? 'Complete': 'Incomplete'
+    task.complete? ? 'Complete': 'Incomplete'
   end
+
+  def user_gravatar(user,size)
+    email_hash = Digest::MD5.hexdigest(user.email.strip.downcase) 
+    gravatar_url = "https://s.gravatar.com/avatar/#{email_hash}?s=#{size}" 
+    gravatar_to_display = image_exists?(gravatar_url) ? gravatar_url : DEFAULT_GRAVATAR
+    image_tag(gravatar_to_display,class:'mx-0',size: size, alt: user.name)
+  end
+
+  private
+
+  def image_exists?(url)
+    begin
+      url = URI.parse(url)
+      http = Net::HTTP.start(url.host, url.port)
+      http.head(url.request_uri)['Content-Type'].start_with? 'image'
+    rescue
+      false
+    end
+  end
+
 end
