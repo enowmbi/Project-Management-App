44c3d6cf2443970bdd6c40c353ce00d3f241de8a
Enow Mbi <benowmbi@yahoo.com>
2020-04-24 02:41:10 +0100

    Set new default gravatar to that in public folder

--- config/initializers/gravatar_image_tag.rb
+++ config/initializers/gravatar_image_tag.rb
@@ -1,5 +1,5 @@
  GravatarImageTag.configure do |config|
-   config.default_image           = :identicon   
+   config.default_image           = 'localhost:3000/default_gravatar.jpeg'
    config.filetype                = nil   
    config.include_size_attributes = true  
    config.rating                  = nil   
