d468565bad3ece3a970a2c752a25ff11c6278f3a
Enow Mbi <benowmbi@yahoo.com>
2020-04-27 12:16:58 +0100

    Remove description from project --resort to using action_text for handling richtext

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_25_101344) do
+ActiveRecord::Schema.define(version: 2020_04_27_111033) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -66,7 +66,6 @@ ActiveRecord::Schema.define(version: 2020_04_25_101344) do
 
   create_table "projects", force: :cascade do |t|
     t.string "name"
-    t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "team_id", null: false
@@ -81,7 +80,7 @@ ActiveRecord::Schema.define(version: 2020_04_25_101344) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "title"
-    t.text "description"
+    t.text "old_description"
     t.string "priority"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
