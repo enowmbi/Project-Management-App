615a9e01b237af0c93faef8d282eb26243286fcd
Enow Mbi <benowmbi@yahoo.com>
2020-04-28 09:42:44 +0100

    Overide create method and include membership addition using data

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -1,13 +1,17 @@
 class InvitationsController < Devise::InvitationsController
 
   def create
-    user = User.new(name: params[:user][:name],email: params[:user][:email])
-    unless User.exists?(user.id)
+    user = User.find_by_email(params[:user][:email])
+    unless user 
+      user = User.new({name: params[:user][:name],email: params[:user][:email]})
       user.skip_confirmation!
       user.save
     end
+
     User.invite!({name: user.name,email: user.email}, current_user)
-    redirect_to team_path(params[:team_id]),notice: 'Invitation has been sent to #{params[:user][:name]}'
+    membership = Membership.new({user_id: User.last.id,team_id: params[:team_id],invitation_status: 'pending'})
+    membership.save
+    redirect_to team_path(params[:team_id]),notice: "Invitation has been sent to #{params[:user][:name]}"
   end
 
 end
