8529782393c7f387efb6d40fe8dc3689628cab60
Enow Mbi <benowmbi@yahoo.com>
2020-04-30 14:38:37 +0100

    Generate links from helper to make index.html.erb of tasks cleaner --wop

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,3 +1,16 @@
 module TasksHelper
   TASK_PRIORITIES = ['High','Medium','Low']
+
+  sortables = %w(title priority complete)
+
+  sortables.each do |sortable|
+    define_method "sort_by_#{sortable}_asc" do 
+      # link_to("<i class='fas fa-sort-up'></i>".html_safe,"/teams/params[:team_id]/sort_by_#{sortable}_asc_team_project_tasks_path(params[:team_id],params[:project_id])".html_safe) 
+    end
+
+    define_method "sort_by_#{sortable}_desc" do 
+      # link_to("<i class='fas fa-sort-down'></i>".html_safe,"sort_by_#{sortable}_asc_team_project_tasks_path(#{sort_params})".html_safe) 
+    end
+  end
+
 end
