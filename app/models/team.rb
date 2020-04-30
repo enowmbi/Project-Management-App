d46244aae4abb3b00c5df7397d943e83c5059f03
Enow Mbi <benowmbi@yahoo.com>
2020-04-30 21:39:34 +0100

    Generate named scopes for activity,title and name

--- app/models/team.rb
+++ app/models/team.rb
@@ -3,8 +3,14 @@ class Team < ApplicationRecord
   has_many :users, through: :memberships
   has_many :projects, dependent: :destroy
 
-  default_scope ->{order(created_at: :desc)}
+  scope :most_recent_first, ->{order(created_at: :desc)}
 
+  sortables = %w(name active owner)
+
+  sortables.each do |sortable|
+    scope "ascending_#{sortable}".to_sym, ->{order("#{sortable} asc")}
+    scope "descending_#{sortable}".to_sym, ->{order("#{sortable} desc")}
+  end
 
   extend FriendlyId
   friendly_id :name, use: :slugged
