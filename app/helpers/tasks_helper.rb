b0fe583bd282578c05fba39c37f92ce2d037182a
Enow Mbi <benowmbi@yahoo.com>
2020-04-30 15:24:40 +0100

    Add helpers for displaying links on index page

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,15 +1,14 @@
 module TasksHelper
   TASK_PRIORITIES = ['High','Medium','Low']
 
-  sortables = %w(title priority complete)
+  sortables = %w(title priority status)
 
   sortables.each do |sortable|
-    define_method "sort_by_#{sortable}_asc" do 
-      # link_to("<i class='fas fa-sort-up'></i>".html_safe,"/teams/params[:team_id]/sort_by_#{sortable}_asc_team_project_tasks_path(params[:team_id],params[:project_id])".html_safe) 
+    define_method "sort_by_#{sortable}_asc" do |team,project| 
+      link_to("<i class='fas fa-sort-up'></i>".html_safe,"/teams/#{team}/projects/#{project}/tasks/sort_by_#{sortable}_asc".html_safe) 
     end
-
-    define_method "sort_by_#{sortable}_desc" do 
-      # link_to("<i class='fas fa-sort-down'></i>".html_safe,"sort_by_#{sortable}_asc_team_project_tasks_path(#{sort_params})".html_safe) 
+    define_method "sort_by_#{sortable}_desc" do |team,project| 
+      link_to("<i class='fas fa-sort-down'></i>".html_safe,"/teams/#{team}/projects/#{project}/tasks/sort_by_#{sortable}_desc".html_safe) 
     end
   end
 
