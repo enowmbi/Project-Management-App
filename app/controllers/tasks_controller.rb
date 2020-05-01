65b757419abbb203a99cc800b6ce52e7298d44c6
Enow Mbi <benowmbi@yahoo.com>
2020-05-01 08:43:59 +0100

    Use most_recent_first scope when displaying all tasks from index page

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -5,7 +5,7 @@ class TasksController < ApplicationController
   # GET /tasks
   # GET /tasks.json
   def index
-    @tasks = Task.all_tasks(@project)
+    @tasks = Task.all_tasks(@project).most_recent_first
   end
 
   # GET /tasks/1
