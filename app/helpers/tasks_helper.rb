3de3a2c8fe5d09933e377a8c87375881244d875f
Enow Mbi <benowmbi@yahoo.com>
2020-05-01 10:18:31 +0100

    Rename helpers to reflect their origin i.e. 'sort_by_task_title_asc' as opposed to 'sort_by_title_asc'

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -4,10 +4,10 @@ module TasksHelper
   sortables = %w(title priority complete)
 
   sortables.each do |sortable|
-    define_method "sort_by_#{sortable}_asc" do |team,project| 
+    define_method "sort_by_task_#{sortable}_asc" do |team,project| 
       link_to("<i class='fas fa-sort-up'></i>".html_safe,"/teams/#{team}/projects/#{project}/tasks/sort_by_#{sortable}_asc".html_safe) 
     end
-    define_method "sort_by_#{sortable}_desc" do |team,project| 
+    define_method "sort_by_task_#{sortable}_desc" do |team,project| 
       link_to("<i class='fas fa-sort-down'></i>".html_safe,"/teams/#{team}/projects/#{project}/tasks/sort_by_#{sortable}_desc".html_safe) 
     end
   end
