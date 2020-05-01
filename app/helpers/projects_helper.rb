051a5693640834c8ce926c7cef06636881592c86
Enow Mbi <benowmbi@yahoo.com>
2020-05-01 10:47:31 +0100

    Generate helpers for the sortable attributes which generate links to sort projects in index.html.erb

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,9 +1,22 @@
 module ProjectsHelper
   def project_activity(project)
-     project.active? ? 'Active' : 'Inactive'
+    project.active? ? 'Active' : 'Inactive'
   end
 
   def project_status(project)
-     project.complete? ? 'Complete' : 'Incomplete'
+    project.complete? ? 'Complete' : 'Incomplete'
   end
+
+  sortables = %w(name due_date active complete)
+
+  sortables.each do |sortable| 
+    define_method "sort_by_project_#{sortable}_asc" do |team|
+      link_to("<i class='fas fa-sort-up'></i>".html_safe, "/teams/#{team}/projects/sort_by_#{sortable}_asc".html_safe) 
+    end
+
+    define_method "sort_by_project_#{sortable}_desc" do |team|
+      link_to("<i class='fas fa-sort-down'></i>".html_safe, "/teams/#{team}/projects/sort_by_#{sortable}_desc".html_safe) 
+    end
+  end
+
 end
