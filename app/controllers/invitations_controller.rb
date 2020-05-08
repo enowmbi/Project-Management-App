559acc7a6949ba1cd17afb120663b79742691403
Enow Mbi <benowmbi@yahoo.com>
2020-05-08 14:05:54 +0100

    Add code to prevent duplicate memberships for same team and member

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -9,8 +9,12 @@ class InvitationsController < Devise::InvitationsController
     end
 
     User.invite!({name: user.name,email: user.email}, current_user)
-    membership = Membership.new({user_id: User.last.id,team_id: params[:team_id]})
-    membership.save
-    redirect_to team_path(params[:team_id]),notice: "Invitation has been sent to #{params[:user][:name]}"
+
+    membership = Membership.find_by_user_id_and_team_id(user.id,params[:team_id])
+    unless membership
+      membership = Membership.new({user_id: user.id,team_id: params[:team_id]})
+      membership.save
+      redirect_to team_path(params[:team_id]),notice: "Invitation has been sent to #{params[:user][:name]}"
+    end
   end
 end
