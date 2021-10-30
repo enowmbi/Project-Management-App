98099439267e1d4338423eefb9b86bbc75a3c7ce
Enow Mbi <benowmbi@yahoo.com>
2021-10-30 10:02:40 +0100

    Fix rubocop offences in project.rb

--- app/models/project.rb
+++ app/models/project.rb
@@ -7,14 +7,14 @@ class Project < ApplicationRecord
   validates :name, presence: true
   validates :name, uniqueness: true
 
-  scope :all_projects, ->(team){where(team_id: team.id)}
-  scope :most_recent_first, ->{order(created_at: :desc)} 
+  scope :all_projects, ->(team) { where(team_id: team.id) }
+  scope :most_recent_first, -> { order(created_at: :desc) }
 
-  sortables = %w(name due_date active complete tasks_count)
+  sortables = %w[name due_date active complete tasks_count]
 
   sortables.each do |sortable|
-    scope "ascending_#{sortable}".to_sym, ->{order("#{sortable} asc")}
-    scope "descending_#{sortable}".to_sym, ->{order("#{sortable} desc")}
+    scope "ascending_#{sortable}".to_sym, -> { order("#{sortable} asc") }
+    scope "descending_#{sortable}".to_sym, -> { order("#{sortable} desc") }
   end
 
   extend FriendlyId
